0f0287df7b0ee947e5a4ee45616fc37b9da8f46c
luoyumo <yumo.luo@hotmail.com>
2019-01-25 10:44:31 +0200

    running the visualisation

--- src/main/java/maze/Main.java
+++ src/main/java/maze/Main.java
@@ -1,13 +1,14 @@
 package maze;
 
-import mazeVisualisation.MazeVisualizer;
+import mazeVisualisation.MazeSolverVisualizer;
 
 public class Main {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String file="labyrinth_101_101.txt";
-        MazeVisualizer vis=new MazeVisualizer(file);
+        String file = "labyrinth_101_101.txt";
+        MazeSolverVisualizer vis = new MazeSolverVisualizer(file);
     }
 }
