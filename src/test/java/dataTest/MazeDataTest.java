29a49744426358afd7ade116799e928f15b21fac
luoyumo <yumo.luo@hotmail.com>
2019-02-01 15:15:18 +0200

    test for MazeData

--- src/test/java/dataTest/MazeDataTest.java
+++ src/test/java/dataTest/MazeDataTest.java
@@ -1,5 +1,6 @@
-package data;
+package dataTest;
 
+import data.MazeData;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,6 +11,8 @@ import static org.junit.Assert.*;
 public class MazeDataTest {
 
     private MazeData data;
+    private final int ROWS = 51;
+    private final int COLUMNS = 31;
 
     public MazeDataTest() {
     }
@@ -24,54 +27,51 @@ public class MazeDataTest {
 
     @Before
     public void setUp() {
-        data = new MazeData("labyrinth_101_101.txt");
+        data = new MazeData(ROWS, COLUMNS);
     }
 
     @After
     public void tearDown() {
     }
-    
-    /**
-     * Should throw exception when trying to read from a maze file whose name is empty
-     */
+
+   
     @Test(expected = Exception.class)
     public void throwExceptionIfFilenameEmptyWhenreadingAMazeFile() {
-        String emptyFilename = null;
-        data = new MazeData(emptyFilename);
+
+        MazeData failedData = new MazeData(20, 30);
     }
     
-    /**
-     * Should throw exception when trying to read from a maze file whose name doesn't exist
-     */
-    @Test(expected = Exception.class)
-    public void throwExceptionIfFilenameIsWrongWhenreadingAMazeFile() {
-        String wrongFilename = "wrongName";
-        data = new MazeData(wrongFilename);
+    @Test
+    public void initialiseCorrcectPattern() {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if (i == 1 && j == 0) {
+                    assertEquals(MazeData.ROAD, data.maze[i][j]);
+                } else if (i == ROWS - 2 && (j == COLUMNS - 1)) {
+                    assertEquals(MazeData.ROAD, data.maze[i][j]);
+                } else if (i % 2 == 1 && j % 2 == 1) {
+                    assertEquals(MazeData.ROAD, data.maze[i][j]);
+                } else {
+                    assertEquals(MazeData.WALL, data.maze[i][j]);
+                }
+            }
+        }
     }
-    
-    /**
-     * Can return the correct numbers of columns and rows
-     */
+
     @Test
     public void canGetRightColumnAndRowNumbers() {
-        int columns = 101;
-        int lines = 101;
-        assertEquals(columns, data.getColumn());
-        assertEquals(lines, data.getRow());
+        assertEquals(COLUMNS, data.getColumn());
+        assertEquals(ROWS, data.getRow());
     }
+
     
-    /**
-     * Should return true when the coordinate pair is in the maze
-     */
     @Test
     public void returnTrueWhenCoordinatePairInMaze() {
         boolean result = data.inArea(3, 3);
         assertTrue(result);
     }
- 
-    /**
-     * Should return false when the coordinate pair is in the outside of the maze
-     */
+
+    
     @Test
     public void returnFalseWhenCoordinatePairOutsideMaze() {
         boolean xOutside = data.inArea(300, 3);
@@ -84,36 +84,24 @@ public class MazeDataTest {
         assertFalse(xNegative);
         assertFalse(yNegative);
     }
+
     
-    /**
-     * Should throw exception when trying to get the coordinate pair which is in the outside of the maze
-     */
     @Test(expected = Exception.class)
     public void throwExceptionIfCoordinatePairOutsideMazeWhenGettingCertainCharFromMaze() {
         char wrongCoordinates = data.getMaze(300, 300);
     }
+
     
-    /**
-     * Can get right character when given correct coordination pair
-     */
     @Test
     public void canGetRightCharacterFromMaze() {
         char character = data.getMaze(2, 2);
-        char wall = '#';
+        char wall = MazeData.WALL;
         assertEquals(wall, character);
-        
-        char character2=data.getMaze(2, 3);
-        char path = ' ';
-        assertEquals(path,character2);
-        
-    }
-    
-    /**
-     * Should throw exception when different rows have different number of characters
-     */
-    @Test(expected = Exception.class)
-    public void throwExceptionWhenChratersNumberInEachRowNotEqual() {
-        data = new MazeData("wrongLabyrinth.txt");
+
+        char character2 = data.getMaze(3, 3);
+        char path = MazeData.ROAD;
+        assertEquals(path, character2);
+
     }
 
 }
