0708f8d3639b970a7778e61c9743b09c81bc64ea
luoyumo <yumo.luo@hotmail.com>
2019-02-01 15:16:22 +0200

    Test for maze generation algorithm

---
+++ src/test/java/algoTest/MazeGeneratorTest.java
@@ -0,0 +1,53 @@
+package algoTest;
+
+import algo.Bfs;
+import algo.PrimGenerator;
+import data.MazeData;
+import mazeVisualisation.MazeFrame;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+public class MazeGeneratorTest {
+
+    private MazeData data;
+    private MazeFrame frame;
+    private Bfs bfs;
+
+    public MazeGeneratorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        int rows = 51;
+        int columns = 71;
+        data = new MazeData(rows, columns);
+        frame = mock(MazeFrame.class);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void mazeGeneratedByPrimHasSolution() {
+        PrimGenerator primGen = new PrimGenerator(data, frame);
+        primGen.generateMaze();
+        bfs = new Bfs(data, frame);
+        data.resetTables();
+        assertTrue(bfs.searchWay());
+    }
+
+}
