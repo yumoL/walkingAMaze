6b3415e22b84c00fb95e06ea3c1ba2550bc86d7d
luoyumo <yumo.luo@hotmail.com>
2019-02-08 15:57:33 +0200

    crude version

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,74 +1,42 @@
-package maze;
+package main;
 
-
+import performanceComparison.Comparator;
 import algo.shortestPathSolver.AstarWithManhattan;
 import algo.shortestPathSolver.Node;
-import data.MazeData;
+import data.GraphData;
 import java.util.HashMap;
 import java.util.HashSet;
-import mazeVisualisation.MazeFrame;
-import mazeVisualisation.MazeVisualizer;
+import graphVisualization.GraphFrame;
+import graphVisualization.GraphVisualizer;
+import java.util.Scanner;
 import util.IndexPriorityQueue;
 import util.MyHashMap;
 
-
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        int rows = 71;
-        int columns = 71;
+        //Here's just a crude version. If you'd like to walk the labyrinth, please set the 'choose'variable as 0.
+        //If you'd like to test the performance of algorithms, please set the 'choose' variable as 1
+        
+        //When walking the labyrinth,, press keyboard 'a' for bfs, 'b' for A* using manhattan disctance, 'c'for A* using euclidean distance and 'd' for A* using squared euclidean distance
+        // please walk the labyrinth in other algorithms before using A* with squared euclidean distance
+        int choose = 1;
+        if (choose == 0) {
+            GraphVisualizer gvis = new GraphVisualizer(101/*rows*/, 101/*columns*/, 0/*which way to generate the graph,0=dfs,1=randomized Prim*/);
+        } else if (choose == 1) {
+//            Comparator comp = new Comparator(101, 101, 0);//Comparator is initialized in the same way as GraphVisualizer
+            comp.testBfs();
+            comp.testAstarManhattan();
+        }
 
-        MazeVisualizer gvis = new MazeVisualizer(rows, columns);
-//        char[][] m = new char[9][9];
+    }
+
+//    public static void printMaze(char[][] m) {
 //        for (int i = 0; i < 9; i++) {
 //            for (int j = 0; j < 9; j++) {
-//                m[i][j] = '#';
+//                System.out.print(m[i][j]);
 //            }
-//
+//            System.out.println("");
 //        }
-//        for (int j = 0; j < 8; j++) {
-//            m[1][j] = ' ';
-//        }
-//        m[2][1] = ' ';
-//        m[2][3] = ' ';
-//        m[2][5] = ' ';
-//        m[2][6] = ' ';
-//        m[3][1]=' ';m[3][2]=' ';m[3][3]=' ';m[3][5]=' ';m[3][7]=' ';
-//        m[4][1]=' ';m[4][3]=' ';m[4][7]=' ';
-//        m[5][1]=' ';m[5][3]=' ';m[5][4]=' ';m[5][5]=' ';m[5][6]=' ';m[5][7]=' ';
-//        m[6][3]=' ';
-//        for(int i=1;i<9;i++){
-//            m[7][i]=' ';
-//        }
-//        printMaze(m);
-//        
-//        MazeData data=new MazeData(9,9);
-//        data.maze=m;
-//        
-//        AstarWithManhattan a=new AstarWithManhattan(data,new MazeFrame("m",50,50));
-//        a.searchWay();
-//        Node n1=new Node(1,16);n1.setF(14);
-//        Node n2=new Node(1,16);n2.setF(16);
-//        Node n3=new Node(1,16);n3.setF(10);
-//        MyHashMap<Node,Integer>map=new MyHashMap<>();
-//        IndexPriorityQueue<Node>pq=new IndexPriorityQueue<>(9);
-//        map.put(n2, 1);
-//        pq.add(2,n1);pq.add(1, n2);
-//        pq.change(2, n3);
-//        System.out.println(pq.pollElement());
-//        System.out.println(pq.pollElement());
-      
-    }
-
-    public static void printMaze(char[][] m) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(m[i][j]);
-            }
-            System.out.println("");
-        }
-    }
+//    }
 }
