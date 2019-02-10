87614aeef00175e508cd9e9ff2d8d2e041890d54
luoyumo <yumo.luo@hotmail.com>
2019-02-11 01:35:12 +0200

    hashmap implementation

--- src/main/java/util/MyHashMap.java
+++ src/main/java/util/MyHashMap.java
@@ -1,112 +1,126 @@
 package util;
-import java.util.TreeMap;
 
-//TODO
-public class MyHashMap<K extends Comparable<K>, V> {
-    //a set of possible capacity, should be prime numbers
-    private final int[] CAPACITY
-            = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593,
-            49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469,
-            12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741};
+import data.Node;
 
-    private static final int MAXTOLERANCE = 10;
-    private static final int MINTOLERANCE = 2;
-    private int capIndex = 0;
+/**
+ * HashMap implementation using chaining
+ */
+public class MyHashMap {
 
-    private TreeMap<K, V>[] hashmap;
-    private int size;
-    private int M;
+    class Entry {
 
-    public MyHashMap(){
-        this.M = CAPACITY[capIndex];
-        size = 0;
-        hashmap = new TreeMap[M];
-        for(int i = 0 ; i < M ; i ++)
-            hashmap[i] = new TreeMap<>();
+        Node key;
+        int value;
+        Entry next;
+
+        public Entry(Node key, int value, Entry next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
-    
+
+    private final int CAPACITY = 12289; //defaulted capacity, should be prime number
+    private Entry[] map;
+
+    public MyHashMap() {
+        map = new Entry[CAPACITY];
+    }
+
     /*
-    Hash the key
-    */
-    public int hash(K key){
-        return (key.hashCode() & 0x7fffffff) % M;
-    }
-
-    public int getSize(){
-        return size;
-    }
-    
-    /**
-     * Put a new pair of key and value. If the key is already in the map then overwrite it
-     * @param key the key of the pair
-     * @param value the value of the pair
+    Hash function
      */
-    public void put(K key, V value){
-        TreeMap<K, V> map = hashmap[hash(key)];
-        if(map.containsKey(key))
-            map.put(key, value);
-        else{
-            map.put(key, value);
-            size ++;
+    private int hash(Node node) {
+        return node.hashCode() % CAPACITY;
+    }
 
-            if(size >= MAXTOLERANCE * M && capIndex + 1 < CAPACITY.length){
-                capIndex ++;
-                resize(CAPACITY[capIndex]);
+    /**
+     * Get value according to the key
+     *
+     * @param key
+     * @return the value to which the specified key is mapped, or -1 if this map
+     * contains no mapping for the key or the key doesn't exist
+     */
+    public int get(Node key) {
+        int h = hash(key);
+        if (map[h] == null) {
+            return -1;
+        }
+        Entry entry = map[h];
+        while (entry != null && !entry.key.equals(key)) {
+            entry = entry.next;
+        }
+        if (entry == null) {
+            return -1;
+        }
+        return entry.value;
+    }
+
+    /**
+     * Associate the specified value with the specified key in this map. If the
+     * map previously contained a mapping for the key, the old value is
+     * replaced.
+     *
+     * @param key key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     */
+    public void put(Node key, int value) {
+        int h = hash(key);
+        if (map[h] == null) {
+            map[h] = new Entry(key, value, null);
+        } else {
+            Entry entry = map[h];
+            while (entry.next != null && !entry.key.equals(key)) {
+                entry = entry.next;
+            }
+            if (entry.key.equals(key)) {
+                entry.value = value;
+            } else {
+                entry.next = new Entry(key, value, null);
             }
         }
     }
-    
-    /**
-     * Remove the pair with given key
-     * @param key the key to be removed
-     * @return value of the pair with given key. If the given key doesn't exit in the map, return null
-     */
-    public V remove(K key){
-        V ret = null;
-        TreeMap<K, V> map = hashmap[hash(key)];
-        if(map.containsKey(key)){
-            ret = map.remove(key);
-            size --;
 
-            if(size < MINTOLERANCE * M && capIndex - 1 >= 0){
-                capIndex --;
-                resize(CAPACITY[capIndex]);
+    /**
+     * Remove the mapping for the specified key from this map if present.
+     *
+     * @param key key whose mapping is to be removed from the map
+     */
+    public void remove(Node key) {
+        int h = hash(key);
+        if (map[h] != null) {
+            Entry prevEntry = null;
+            Entry entry = map[h];
+            while (entry.next != null && !entry.key.equals(key)) {
+                prevEntry = entry;
+                entry = entry.next;
+            }
+            if (entry.key.equals(key)) {
+                if (prevEntry == null) {
+                    map[h] = entry.next;
+                } else {
+                    prevEntry.next = entry.next;
+                }
             }
         }
-        return ret;
     }
-    
-    /*
-    Define if the map contains the given key
-    */
-    public boolean containsKey(K key){
-        return hashmap[hash(key)].containsKey(key);
-    }
-    
-    /*
-    Get the value of the pair with given key
-    */
-    public V get(K key){
-        return hashmap[hash(key)].get(key);
-    }
-    
+
     /**
-     * Resize the hashmap
-     * @param newM the new capacity of the hashmap
+     * Determine if the map contains the given key
+     *
+     * @param key The key whose presence in this map is to be tested
+     * @return true if this map contains a mapping for the specified key.
+     * Otherwise false
      */
-    private void resize(int newM){
-        TreeMap<K, V>[] newHashTable = new TreeMap[newM];
-        for(int i = 0 ; i < newM ; i ++)
-            newHashTable[i] = new TreeMap<>();
-
-        int oldM = M;
-        this.M = newM;
-        for(int i = 0 ; i < oldM ; i ++){
-            TreeMap<K, V> map = hashmap[i];
-            for(K key: map.keySet())
-                newHashTable[hash(key)].put(key, map.get(key));
+    public boolean containsKey(Node key) {
+        int h = hash(key);
+        if (map[h] == null) {
+            return false;
         }
-
-        this.hashmap = newHashTable;
+        Entry entry = map[h];
+        while (entry.next != null && !entry.key.equals(key)) {
+            entry = entry.next;
+        }
+        return entry.key.equals(key);
     }
 }
