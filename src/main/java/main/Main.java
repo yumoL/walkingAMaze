c087accfd31e27d1fbd813791507cc20701848b4
luoyumo <yumo.luo@hotmail.com>
2019-02-20 15:53:33 +0200

    removed unneeded codes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,21 +1,12 @@
 package main;
 
-import performanceComparison.Comparator;
-
 public class Main {
 
     public static void main(String[] args) {
-        int choose = 0;
-        if (choose == 0) {
-            App app = new App();
-            app.run();
-        } else {
-            Comparator comp = new Comparator(3, 3, 1);//Comparator is initialized in the same way as GraphVisualizer
-            comp.testBfs();
-            comp.testAstarManhattan();
-            comp.testAstarEu();
-            comp.testAstarSquaredEu();
-        }
+
+        App app = new App();
+        app.run();
+
     }
 
 }
